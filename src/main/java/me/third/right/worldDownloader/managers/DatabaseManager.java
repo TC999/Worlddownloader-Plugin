1981d77f9766cd03186f3e95b4fc025fe5c3d8cd
third <ethanamt2001@gmail.com>
2022-10-22 21:45:30 +0100

    1.2 Improved DatabaseManager

--- src/main/java/me/third/right/worldDownloader/managers/DatabaseManager.java
+++ src/main/java/me/third/right/worldDownloader/managers/DatabaseManager.java
@@ -13,11 +13,13 @@ import net.minecraft.block.BlockAir;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
+import org.jetbrains.annotations.NotNull;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class DatabaseManager {
     protected final Minecraft mc = Minecraft.getMinecraft();
@@ -40,160 +42,143 @@ public class DatabaseManager {
 
     public void onTick() {
         if(chunkHistory.size() > 400) {
-            chunkHistory.clear();
+            Collections.reverse((List<?>) chunkHistory);
+            for(int i = 0; i < 300; i++) {
+                chunkHistory.remove();
+            }
         }
 
         if(newChunkHistory.size() > 400) {
-            newChunkHistory.clear();
-        }
-    }
-
-    public void storeChunkInfo(Chunk chunk) {//TODO add Hash checks to see if the chunk has changed if not then don't store it.
-        if(isClosing) return;
-        StoreTransaction transaction = entityStore.beginTransaction();
-        final int id = Objects.hashCode(chunk.x) ^ Objects.hashCode(chunk.z);
-        final int hash = ChunkUtils.createChunkHash(chunk);
-
-        final Pair<Integer, Integer> histEntry = new Pair<>(id, hash);
-        if(chunkHistory.contains(histEntry)) {
-            transaction.abort();
-            return;
-        }
-
-        //Block Counts
-        try {
-            do {
-                //Block
-                final Map<Block, Pair<Integer, HashSet<BlockPos>>> blockCount = new HashMap<>();
-                for (int x = 0; x < 16; x++) {
-                    for (int y = 0; y < 256; y++) {
-                        for (int z = 0; z < 16; z++) {
-                            final Block block = chunk.getBlockState(x, y, z).getBlock();
-                            if (block instanceof BlockAir) continue;
-                            if (blockCount.containsKey(block)) {
-                                final Pair<Integer, HashSet<BlockPos>> pair = blockCount.get(block);
-                                pair.setFirst(pair.getFirst() + 1);
-                                pair.getSecond().add(new BlockPos(chunk.x * 16 + x, y, chunk.z * 16 + z));
-                            } else {
-                                blockCount.put(block, new Pair<>(1, new HashSet<>(Collections.singletonList(new BlockPos(chunk.x * 16 + x, y, chunk.z * 16 + z)))));
-                            }
-                        }
-                    }
-                }
-
-                Entity entity = transaction.newEntity("Block");
-                entity.setProperty("id", id);;
-                entity.setProperty("chunkX", chunk.x);
-                entity.setProperty("chunkZ", chunk.z);
-                entity.setProperty("date", dtf.format(LocalDateTime.now()));
-                entity.setProperty("dimension", mc.player.dimension);
-                entity.setProperty("hash", hash);
-
-                //#name:count@x,y,z|x,y,z
-                //Split by # then by : then by @ then by ,
-                final StringBuilder blockString = new StringBuilder();
-                for (Map.Entry<Block, Pair<Integer, HashSet<BlockPos>>> entry : blockCount.entrySet()) {
-                    blockString.append(String.format("#%s:%d@", entry.getKey().getLocalizedName(), entry.getValue().getFirst()));
-
-                    for (BlockPos pos : entry.getValue().getSecond()) {
-                        blockString.append(String.format("%d,%d,%d|", pos.getX(), pos.getY(), pos.getZ()));
-                    }
-                }
-                entity.setProperty("blocks", blockString.toString());
-
-                if(transaction != entityStore.getCurrentTransaction()) {
-                    transaction = null;
-                    break;
-                }
-            } while (!transaction.flush());
-        } finally {
-            chunkHistory.add(histEntry);
-            if(transaction != null) {
-                transaction.abort();
+            Collections.reverse((List<?>) newChunkHistory);
+            for(int i = 0; i < 300; i++) {
+                newChunkHistory.remove();
             }
         }
     }
 
+    public void storeChunkInfo(Chunk chunk) {
+        if(isClosing) return;
+        entityStore.executeInTransaction(txn -> {
+            final int id = Objects.hashCode(chunk.x) ^ Objects.hashCode(chunk.z);
+            final int hash = ChunkUtils.createChunkHash(chunk);
+            //Block
+            final Map<Block, Pair<Integer, HashSet<BlockPos>>> blockCount = new HashMap<>();
+            for (int x = 0; x < 16; x++) {
+                for (int y = 0; y < 256; y++) {
+                    for (int z = 0; z < 16; z++) {
+                        final Block block = chunk.getBlockState(x, y, z).getBlock();
+                        if (block instanceof BlockAir) continue;
+                        if (blockCount.containsKey(block)) {
+                            final Pair<Integer, HashSet<BlockPos>> pair = blockCount.get(block);
+                            pair.setFirst(pair.getFirst() + 1);
+                            pair.getSecond().add(new BlockPos(chunk.x * 16 + x, y, chunk.z * 16 + z));
+                        } else {
+                            blockCount.put(block, new Pair<>(1, new HashSet<>(Collections.singletonList(new BlockPos(chunk.x * 16 + x, y, chunk.z * 16 + z)))));
+                        }
+                    }
+                }
+            }
+
+            Entity entity = txn.newEntity("Block");
+            entity.setProperty("id", id);;
+            entity.setProperty("chunkX", chunk.x);
+            entity.setProperty("chunkZ", chunk.z);
+            entity.setProperty("date", dtf.format(LocalDateTime.now()));
+            entity.setProperty("dimension", mc.player.dimension);
+            entity.setProperty("hash", hash);
+
+            //#name:count@x,y,z|x,y,z
+            //Split by # then by : then by @ then by ,
+            final StringBuilder blockString = new StringBuilder();
+            for (Map.Entry<Block, Pair<Integer, HashSet<BlockPos>>> entry : blockCount.entrySet()) {
+                blockString.append(String.format("#%s:%d@", entry.getKey().getLocalizedName(), entry.getValue().getFirst()));
+
+                for (BlockPos pos : entry.getValue().getSecond()) {
+                    blockString.append(String.format("%d,%d,%d|", pos.getX(), pos.getY(), pos.getZ()));
+                }
+            }
+            entity.setProperty("blocks", blockString.toString());
+
+        });
+    }
+
     public long getBlockCount() {
         if(isClosing) return 0;
-        return entityStore.beginReadonlyTransaction().getAll("Block").size();
+        AtomicLong returnVar = new AtomicLong();
+        entityStore.executeInReadonlyTransaction(txn -> {
+            final EntityIterable all = txn.getAll("Block");
+            returnVar.set(all.size());
+        });
+        return returnVar.get();
     }
 
     public HashMap<String , BigInteger>  calcTotals() {
         if(isClosing) return null;
         HashMap<String, BigInteger> blockCount = new HashMap<>();
-        final EntityIterable blocks = entityStore.beginReadonlyTransaction().getAll("Block");
-        for (Entity block : blocks) {
-            final String[] blocksSplit = String.valueOf(block.getProperty("blocks")).split("#");
-            for (String blockSplit : blocksSplit) {
-                if (blockSplit.isEmpty()) continue;
-                final String[] blockSplitSplit = blockSplit.split(":");
-                final String blockName = blockSplitSplit[0];
-                final int blockCountInt = Integer.parseInt(blockSplitSplit[1].split("@")[0]);
-                if (blockCount.containsKey(blockName)) {
-                    blockCount.put(blockName, blockCount.get(blockName).add(BigInteger.valueOf(blockCountInt)));
-                } else {
-                    blockCount.put(blockName, BigInteger.valueOf(blockCountInt));
+        entityStore.executeInReadonlyTransaction(txn -> {
+            final EntityIterable blocks =  txn.getAll("Block");
+            for (Entity block : blocks) {
+                final String[] blocksSplit = String.valueOf(block.getProperty("blocks")).split("#");
+                for (String blockSplit : blocksSplit) {
+                    if (blockSplit.isEmpty()) continue;
+                    final String[] blockSplitSplit = blockSplit.split(":");
+                    final String blockName = blockSplitSplit[0];
+                    final int blockCountInt = Integer.parseInt(blockSplitSplit[1].split("@")[0]);
+                    if (blockCount.containsKey(blockName)) {
+                        blockCount.put(blockName, blockCount.get(blockName).add(BigInteger.valueOf(blockCountInt)));
+                    } else {
+                        blockCount.put(blockName, BigInteger.valueOf(blockCountInt));
+                    }
                 }
             }
-        }
+        });
+
         return blockCount;
     }
 
     // * New Chunks START
-    public void storeNewChunk(int x, int z) {//TODO change chunk location to chunkX and chunkZ instead of x and z
+    public void storeNewChunk(int x, int z) {
         if(isClosing) return;
-        StoreTransaction transaction = entityStore.beginTransaction();
-        final int id = Objects.hashCode(x) ^ Objects.hashCode(z);
-
-        if(newChunkHistory.contains(id)) {
-            transaction.abort();
-            return;
-        }
-
-        try {
-            do {
-                final Entity newChunk = transaction.newEntity("NewChunk");
-                newChunk.setProperty("id", id);
-                newChunk.setProperty("chunkX", x);
-                newChunk.setProperty("chunkZ", z);
-                newChunk.setProperty("dimension", mc.player.dimension);
-                newChunk.setProperty("date", dtf.format(LocalDateTime.now()));
-
-                if(transaction != entityStore.getCurrentTransaction()) {
-                    transaction = null;
-                    break;
-                }
-            } while (!transaction.flush());
-        } finally {
-            newChunkHistory.add(id);
-            if(transaction != null) {
-                transaction.abort();
-            }
-        }
+        entityStore.executeInTransaction(txn -> {
+            final int id = Objects.hashCode(x) ^ Objects.hashCode(z);
+            final Entity newChunk = txn.newEntity("NewChunk");
+            newChunk.setProperty("id", id);
+            newChunk.setProperty("chunkX", x);
+            newChunk.setProperty("chunkZ", z);
+            newChunk.setProperty("dimension", mc.player.dimension);
+            newChunk.setProperty("date", dtf.format(LocalDateTime.now()));
+        });
     }
 
     public ValueStore[] getEntry(String entity, int id) {
         if(isClosing) return null;
-        final EntityIterable entries = entityStore.beginReadonlyTransaction().getAll(entity);
         final List<ValueStore> valueStores = new ArrayList<>();
-        for (Entity entry : entries) {
-            if(Objects.equals(entry.getProperty("id"), id)) {
-                Pair<String, String>[] properties = new Pair[entry.getPropertyNames().size()];
-                int index = 0;
-                for(String name : entry.getPropertyNames()) {
-                    properties[index] = new Pair<>(name, String.valueOf(entry.getProperty(name)));
-                    index++;
+        entityStore.executeInReadonlyTransaction(txn -> {
+            final EntityIterable entries = txn.getAll(entity);
+            for (Entity entry : entries) {
+                if(Objects.equals(entry.getProperty("id"), id)) {
+                    Pair<String, String>[] properties = new Pair[entry.getPropertyNames().size()];
+                    int index = 0;
+                    for(String name : entry.getPropertyNames()) {
+                        properties[index] = new Pair<>(name, String.valueOf(entry.getProperty(name)));
+                        index++;
+                    }
+                    valueStores.add(new ValueStore(entity, properties));
                 }
-                valueStores.add(new ValueStore(entity, properties));
             }
-        }
+        });
+
         return valueStores.toArray(new ValueStore[0]);
     }
 
     public long getNewChunkCount() {
         if(isClosing) return 0;
-        return entityStore.beginReadonlyTransaction().getAll("NewChunk").size();
+        AtomicLong returnVar = new AtomicLong();
+        entityStore.executeInReadonlyTransaction(txn -> {
+            final EntityIterable all = txn.getAll("NewChunk");
+            returnVar.set(all.size());
+        });
+        return returnVar.get();
     }
     // * New Chunks END
 
